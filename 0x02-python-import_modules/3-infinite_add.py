fa570c85ac887540bbe25cd01ce82a85129f8c3f
CHARLESUKE <ukcharleay@gmail.com>
2023-11-30 04:09:47 -0800

    0x02-python-import_modules5

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
 
     total = 0
     for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + l]
+        total += int(sys.argv[i + l])
         print("{}".format(total))
