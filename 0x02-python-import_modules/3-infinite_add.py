cce7239156562229541302c1c4a91a2943639ba7
CHARLESUKE <ukcharleay@gmail.com>
2023-11-30 04:03:20 -0800

    0x02-python-import_modules4

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys, math
-    result = 0
-    for i in sys.argv:
-        result += int(i)
-        print("{}".format(result))
+    """print the addition of all arguments."""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + l]
+        print("{}".format(total))
